3695c3143da69975cf8191a7d1f4474cf1ac901b
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-08-28 16:13:40 +0300

    added navbar and login + logout

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -13,22 +13,39 @@ export const useAuthStore = create((set) => ({
 			const res = await axiosInstance.get("/auth/check");
 			set({ authUser: res.data });
 		} catch (err) {
-            console.log('Error in checkAuth ', err.message)
+			console.log("Error in checkAuth ", err.message);
 			set({ authUser: null });
 		} finally {
-            set ({isCheckingAuth: false})
-        }
+			set({ isCheckingAuth: false });
+		}
 	},
 
-    register: async (data) => {
-        try {
-           const res = await axiosInstance.post("/auth/register", data);
-           set({authUser: res.data})
-           toast.success("Account Created successfully");
+	register: async (data) => {
+		try {
+			const res = await axiosInstance.post("/auth/register", data);
+			set({ authUser: res.data });
+			toast.success("Account Created successfully");
+		} catch (err) {
+			toast.error(err.response.data.message);
+		}
+	},
+	login: async (data) => {
+		try {
+			const res = await axiosInstance.post("auth/login", data);
+			set({ authUser: res.data });
+			toast.success("Logged In successfully");
+		} catch (err) {
+			toast.error(err.response.data.message);
+		}
+	},
 
-        } catch  (err){
-            toast.error(err.response.data.message)
-
-        }
-    }
+	logout: async () => {
+		try {
+			await axiosInstance.post("auth/logout");
+			set({ authUser: null });
+			toast.success("Loggout out successfully");
+		} catch (err) {
+			toast.error(err.response.data.message);
+		}
+	},
 }));
