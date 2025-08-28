5e18993b024ce6fd3f87dc99b4c9ab2ceb6eb837
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-08-28 14:21:05 +0300

    installed cors and lucide-react for icons and spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,28 @@ import RegisterPage from "./pages/RegisterPage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import SettingsPage from "./pages/SettingsPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
+import { useAuthStore } from "./store/useAuthStore.js"
+import { useEffect } from "react"
+import {Loader} from 'lucide-react';
 
 
 function App() {
+
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
 
   return (
     <div>
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/messageRouter.js";
+import cors from 'cors'
 
 const app = express();
 
@@ -11,6 +12,11 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true
+}))
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter)
