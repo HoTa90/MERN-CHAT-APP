d29a6a3f245dc62ea3b7ca876a34447edad687f8
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-08-31 19:02:18 +0300

    added homepage and noChatSelected

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoSelectedChat from "../components/NoSelectedChat.jsx";
+import SideBar from "../components/SideBar.jsx";
+import { useChatStore } from "../store/useChatStore.js";
+
 export default function HomePage() {
-   return (
-       <>
-       </>
-   );
+	const { selectedUser } = useChatStore();
+
+	return (
+		<div className="h-screen bg-base-200">
+			<div className="flex items-center justify-center pt-20 px-4">
+				<div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+                    <div className="flex h-full rounded-lg overflow-hidden">
+                        <SideBar />
+
+                        {!selectedUser ? <NoSelectedChat/> : <ChatContainer/>}
+                    </div>
+                </div>
+			</div>
+		</div>
+	);
 }
