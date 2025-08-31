502ebb4b08b50449f54819667420613d0c973546
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-08-31 18:33:02 +0300

    added zustand context for msgs

---
+++ client/src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+	messages: [],
+	users: [],
+	selectedUser: null,
+	isUsersLoading: false,
+	isMessagesLoading: false,
+
+	getUsers: async () => {
+		set({ isUsersLoading: true });
+		try {
+			const res = await axiosInstance.get("/messages/users");
+			set({ users: res.data });
+		} catch (err) {
+			console.log("Error in getUsers ", err.message);
+			toast.error(err.response.data.massage);
+		} finally {
+			set({ isUsersLoading: false });
+		}
+	},
+
+	getMessages: async (userId) => {
+		set({ isMessagesLoading: true });
+		try {
+			const res = await axiosInstance.get(`/messages/${userId}`);
+			set({ messages: res.data });
+		} catch (err) {
+			console.log("Error in getMessages ", err.message);
+			toast.error(err.response.data.massage);
+		} finally {
+			set({ isMessagesLoading: false });
+		}
+	},
+}));
