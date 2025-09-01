8378ca13d62e7f0876618c66b57e0a3497779f89
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-09-01 11:53:05 +0300

    added socket.io-client to client

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,17 +1,22 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
-export const useAuthStore = create((set) => ({
+const BASE_URL = "http://localhost:5001";
+
+export const useAuthStore = create((set, get) => ({
 	authUser: null,
 	isUpdatingProfile: false,
 	isCheckingAuth: true,
 	onlineUsers: [],
+	socket: null,
 
 	checkAuth: async () => {
 		try {
 			const res = await axiosInstance.get("/auth/check");
 			set({ authUser: res.data });
+			get().connectSocket();
 		} catch (err) {
 			console.log("Error in checkAuth ", err.message);
 			set({ authUser: null });
@@ -25,15 +30,17 @@ export const useAuthStore = create((set) => ({
 			const res = await axiosInstance.post("/auth/register", data);
 			set({ authUser: res.data });
 			toast.success("Account Created successfully");
+			get().connectSocket();
 		} catch (err) {
 			toast.error(err.response.data.message);
 		}
 	},
 	login: async (data) => {
 		try {
-			const res = await axiosInstance.post("auth/login", data);
+			const res = await axiosInstance.post("/auth/login", data);
 			set({ authUser: res.data });
 			toast.success("Logged In successfully");
+			get().connectSocket();
 		} catch (err) {
 			toast.error(err.response.data.message);
 		}
@@ -44,6 +51,7 @@ export const useAuthStore = create((set) => ({
 			await axiosInstance.post("/auth/logout");
 			set({ authUser: null });
 			toast.success("Loggout out successfully");
+			get().disconnectSocket();
 		} catch (err) {
 			toast.error(err.response.data.message);
 		}
@@ -56,15 +64,30 @@ export const useAuthStore = create((set) => ({
 			const res = await axiosInstance.put("/auth/update-profile", data);
 			set({ authUser: res.data });
 			toast.success("Profile image updated successfully!");
-			return { user: res.data }; 
+			return { user: res.data };
 		} catch (err) {
 			console.error("error in updateProfile", err.message);
 			const msg =
 				err.response?.data?.message ?? "Failed to update profile";
 			toast.error(msg);
-			return { error: msg }; 
+			return { error: msg };
 		} finally {
 			set({ isUpdatingProfile: false });
+		}
+	},
+
+	connectSocket: () => {
+		const { authUser } = get();
+		if (!authUser || get().socket?.connected) {
+			return;
+		}
+		const socket = io(BASE_URL);
+		socket.connect();
+		set({ socket: socket });
+	},
+	disconnectSocket: () => {
+		if (get().socket?.connected) {
+			get().socket.disconnect();
 		}
 	},
 }));
