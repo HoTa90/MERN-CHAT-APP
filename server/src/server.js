a554dcf58f29a37d98be2ca3427d50936c92e420
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-09-01 17:48:37 +0300

    added deploy scripts and changed url for production

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,12 @@ import { app, server } from "./lib/socket.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/messageRouter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +25,13 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+	});
+}
 
 server.listen(PORT, () => {
 	console.log(`Server is listening on http://localhost:${PORT}...`);
