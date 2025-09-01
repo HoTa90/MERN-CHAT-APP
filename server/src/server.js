805c9521cb35b5363f4b5e18c97f69eaf632311e
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-09-01 11:28:53 +0300

    added socket io to the server

--- server/src/server.js
+++ server/src/server.js
@@ -2,28 +2,28 @@ import express from "express";
 import authRouter from "./routes/authRouter.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/messageRouter.js";
-import cors from 'cors'
-
-const app = express();
+import cors from "cors";
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-	origin: "http://localhost:5173",
-	credentials: true
-}))
-
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
 
 app.use("/api/auth", authRouter);
-app.use("/api/messages", messageRouter)
+app.use("/api/messages", messageRouter);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server is listening on http://localhost:${PORT}...`);
 	connectDB();
 });
